de3ac46b7834f5fb509447209f27fa3c4f9d84c5
Jakub Kolčář <j.kolcar@raynet.cz>
2024-11-18 22:08:39 +0100

    Refactor DeleteFeature imports; remove unused HEADER_ROWS reference

--- src/features/delete_feature.rs
+++ src/features/delete_feature.rs
@@ -1,6 +1,6 @@
 use crossterm::{cursor, event::{self, KeyCode, KeyEvent}, queue, style::{Print, Stylize}, terminal::{self, ClearType}};
 
-use crate::{files_view::{FilesView, FOOTER_ROWS, HEADER_ROWS}, terminal_ui::{FeatureTrait, TerminalUI}};
+use crate::{files_view::{FilesView, FOOTER_ROWS}, terminal_ui::{FeatureTrait, TerminalUI}};
 
 
 pub struct DeleteFeature {
